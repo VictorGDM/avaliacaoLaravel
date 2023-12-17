cea659a2fba8f3b0e7aa4bf4b24f813db9449400
Victor Machado <vgdiasmachado@gmail.com>
2023-12-17 17:29:10 -0300

    Adição da regra de unique para o campo 'name' de Roles.

--- app/Http/Requests/StoreRoleRequest.php
+++ app/Http/Requests/StoreRoleRequest.php
@@ -22,7 +22,7 @@ class StoreRoleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|min:3|max:60'
+            'name' => 'required|string|min:3|max:60|unique:roles,name'
         ];
     }
 }
