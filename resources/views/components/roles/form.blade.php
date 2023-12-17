cb5407688e2caa6a7f27a63c8963b710fb2f02e0
Victor Machado <vgdiasmachado@gmail.com>
2023-12-17 17:31:43 -0300

    Alteração da lista de exibção dos cargos na view index e implementação da rota cargo na navegação.

--- resources/views/components/roles/form.blade.php
+++ resources/views/components/roles/form.blade.php
@@ -7,14 +7,17 @@
 
     <div class="mb-4">
         <label for="name" class="block text-sm font-medium text-gray-600">Nome:</label>
-        <input type="text"
-               id="name"
-               name="name"
-               class="mt-1 p-2 border rounded-md w-full"
-               @isset($name)value="{{ $name }}"@endisset>
+        <input type="text" id="name" name="name" class="mt-1 p-2 border rounded-md w-full"
+        @isset($name)value="{{ $name }}"@endisset>
     </div>
 
     <x-primary-button>
-        <a type="submit" class="bg-blue-500 text-black rounded">Adicionar</a>
+        <a type="submit" class="bg-blue-500 text-black rounded">
+            @if (@isset($name))
+                Editar
+            @else
+                Adicionar
+            @endif
+        </a>
     </x-primary-button>
 </form>
--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -11,46 +11,36 @@
         </div>
     </x-slot>
 
-    <div class="list-disc">
-        <ul>
-            @foreach ($roles as $role)
-                <li class="flex items-center justify-between border-b py-2">
-
-                    <span>{{ $role->name }}</span>
-
-                    <div class="flex items-center space-x-2">
-                        <form action="{{ route('roles.edit', $role->id) }}" method="post" class="inline">
-                            @csrf
-                            @method('PUT')
-                            <button class="bg-green-500 text-white px-3 py-1 rounded-md">
-                                Editar
-                            </button>
-                        </form>
-
-                        <form action="{{ route('roles.destroy', $role->id) }}" method="post" class="inline">
-                            @csrf
-                            @method('DELETE')
-                            <button class="bg-red-500 text-white px-3 py-1 rounded-md">
-                                Deletar
-                            </button>
-                        </form>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="list-disc">
+                        <ul>
+                            @foreach ($roles as $role)
+                                <li class="flex items-center justify-between border-b py-2">
+                                    <span>{{ $role->name }}</span>
+                                    <div class="flex items-center space-x-2">
+                                        <a href="{{ route('roles.edit', $role->id) }}" class="btn btn-primary btn-sm">
+                                            <button class="bg-green-500 text-white px-3 py-1 rounded-md">
+                                                Editar
+                                            </button>
+                                        </a>
+                                        <form action="{{ route('roles.destroy', $role->id) }}" method="post"
+                                            class="inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="bg-red-500 text-white px-3 py-1 rounded-md">
+                                                Deletar
+                                            </button>
+                                        </form>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
                     </div>
-                </li>
-            @endforeach
-        </ul>
-        {{-- <ul class="list-disc">
-            @foreach ($roles as $role)
-                <li class="py-3 flex items-center justify-between">
-                    <span class="text-lg">{{ $role->name }}</span>
-
-                    <div class="flex items-center space-x-2">
-                        <a href="{{ route('roles.edit', $role->id) }}" class="text-blue-500">
-                            Editar
-                        </a>                        
-                    </div>
-                </li>
-            @endforeach
-        </ul> --}}
-
+                </div>
+            </div>
+        </div>
     </div>
 </x-app-layout>
