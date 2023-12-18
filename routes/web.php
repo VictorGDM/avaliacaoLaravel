0302384361296fd1c9eca165f46297aca5aeb53f
Victor Machado <vgdiasmachado@gmail.com>
2023-12-17 21:05:24 -0300

    Adicionando Middleware para verificação de cargo administrador.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,7 +21,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $users = User::all();
+    return view('dashboard', compact('users'));
 })->middleware(['auth'])->name('dashboard'); //Colocar middleware 'verified', caso queira verificar email.
 
 Route::middleware('auth')->group(function () {
@@ -29,6 +31,17 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('/roles', RoleController::class)->middleware('auth');
+Route::resource('/roles', RoleController::class)->middleware(['auth', 'admin']);
+
+Route::get('/users/{id}/edit', function($id)  {
+    $user = User::find($id);
+    return view('users.edit');
+})->name('users.edit');
+
+Route::delete('/users/{id}', function($id)  {
+    $user = User::find($id);
+    $user->delete();
+    return redirect()->route('dashboard');
+})->name('users.destroy');
 
 require __DIR__.'/auth.php';
